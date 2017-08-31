e6eb82341be98f31de880e77307e923bb153da8d
andip71 <andreasp@gmx.de>
2017-08-31 12:11:20 +0200

    boeffla_wl_blocker: update to wakelock blocker driver v1.1.0
    There are now two lists:
    - the previously existing list of user defined wakelocks to block
    - a new list called "wakelock_blocker_default" which comes prepopulated with the most common
      and safe wakelocks to block:
    
        qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK
    
    A combination of both wakelock lists will be blocked finally.
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Apr 23 04:13:49 CEST 2021"
+#define LINUX_COMPILE_BY "Rogue"
+#define LINUX_COMPILE_HOST "TheMoxes"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project 9829f5e6b1bca9b61efc629770d28bb9014dec45), GNU ld (GNU Binutils) 2.36.1"
