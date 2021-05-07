fa72a55bc36527a00dde49ae68d893ba58f9e130
rogue <phennaux115@gmail.com>
2021-05-07 22:33:20 +0200

    treewide: cleanup build & rebrand to Afterlife

--- build_kernel_proton.sh
+++ build_kernel_proton.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-KERNEL_DEFCONFIG=mox_defconfig
+KERNEL_DEFCONFIG=afterlife_defconfig
 
-export HOME=/home/takemura
+export HOME=/home/rogue
 export CLANG_PATH=$HOME/proton-clang/bin
 export PATH="$CLANG_PATH:$PATH"
 export ARCH=arm64
@@ -13,13 +13,13 @@ export DTC_EXT=$(pwd)/tools/dtc
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 export KBUILD_BUILD_USER=Rogue
-export KBUILD_BUILD_HOST=TheMox
+export KBUILD_BUILD_HOST=TheAfterlife
 
-MOXES_KERNEL_NAME="MoxKernel_"
-MOXES_KERNEL_VERSION="R1"
-ARASAKA_BUILD_DATE="_$(date +%d.%m.%y-%H:%M)"
+KERNEL_NAME="AfterlifeKernel_"
+KERNEL_VERSION="Mistral"
+BUILD_DATE="_$(date +%d.%m.%y-%H:%M)"
 
-IMGNAME=$MOXES_KERNEL_NAME$MOXES_KERNEL_VERSION$ARASAKA_BUILD_DATE
+IMGNAME=$KERNEL_NAME$KERNEL_VERSION$BUILD_DATE
 
 mkbootimg=~/tools/mkbootimg/mkbootimg.py
 ramdisk=~/tools/samsung/ramdisk.cpio.empty
@@ -32,12 +32,12 @@ echo " Kernel defconfig is set to $KERNEL_DEFCONFIG"
 echo
 echo " Setting defconfig"
 echo
-make O=out ARCH=arm64 CC=clang $KERNEL_DEFCONFIG -j$(nproc --all)
+make O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip $KERNEL_DEFCONFIG -j$(nproc --all)
 
 echo
 echo " Compiling"
 echo
-make O=out ARCH=arm64 CC=clang -j$(nproc --all)
+make O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all)
 
 echo
 echo "Building Kernel Image"
@@ -59,7 +59,7 @@ $mkbootimg \
     -o $IMGNAME.img
 
 echo
-echo " Verify Image.gz.dtb"
+echo " Verify Image.gz.dtb, dtbo.img and boot.img"
 echo
 ls $PWD/out/arch/arm64/boot/Image.gz-dtb
 ls $PWD/out/arch/arm64/boot/dtbo.img
@@ -68,5 +68,5 @@ ls -al $IMGNAME.img
 END=$(date +"%s")
 DIFF=$((END - START))
 echo
-echo -e " Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+echo " Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
 echo
