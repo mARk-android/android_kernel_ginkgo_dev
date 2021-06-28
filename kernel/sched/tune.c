ee1a39576f16e2441b2e45c8fa48d5813b512354
mARk <mark@linux.pl>
2021-06-28 19:21:08 +0200

    build: fixed warning: unused declaration and sequence

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -661,10 +661,10 @@ int schedtune_prefer_idle(struct task_struct *p)
 static u64
 prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
+	struct schedtune *st = css_st(css);
+	
 	if (is_battery_saver_on())
 		return 0;
-
-	struct schedtune *st = css_st(css);
 
 	return st->prefer_idle;
 }
@@ -682,10 +682,10 @@ prefer_idle_write(struct cgroup_subsys_state *css, struct cftype *cft,
 static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
+	struct schedtune *st = css_st(css);
+	
 	if (is_battery_saver_on())
 		return 0;
-
-	struct schedtune *st = css_st(css);
 
 	return st->boost;
 }
