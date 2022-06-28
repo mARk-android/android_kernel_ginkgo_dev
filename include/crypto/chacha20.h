d01c34daa52e576f221f1a3e46ca3cd99cb4d18c
mARk <r3066.funtab@gmail.com>
2022-06-28 13:10:45 +0200

    Revert "UPSTREAM: crypto: chacha20 - Fix keystream alignment for chacha20_block()"
    This reverts commit 6c6087bd8b1ff38e5403c27bce5518146231ae80.

--- include/crypto/chacha20.h
+++ include/crypto/chacha20.h
@@ -19,7 +19,6 @@
 
 #define CHACHA20_KEY_SIZE	32
 #define CHACHA20_BLOCK_SIZE	64
-#define CHACHA20_BLOCK_WORDS	(CHACHA20_BLOCK_SIZE / sizeof(u32))
 
 /* 192-bit nonce, then 64-bit stream position */
 #define XCHACHA20_IV_SIZE	32
@@ -28,7 +27,7 @@ struct chacha20_ctx {
 	u32 key[8];
 };
 
-void chacha20_block(u32 *state, u32 *stream);
+void chacha20_block(u32 *state, void *stream);
 void crypto_chacha20_init(u32 *state, struct chacha20_ctx *ctx, u8 *iv);
 
 int crypto_chacha20_setkey(struct crypto_skcipher *tfm, const u8 *key,
